11b93bdd6592a24d47cd2bf802505d2bf6c3f9f2
trlla2 <116338442+trlla2@users.noreply.github.com>
2023-10-16 18:27:37 +0200

    builder           наша свобода

--- app/src/main/java/com/example/apuntesandriod/screens/MainActivity.kt
+++ app/src/main/java/com/example/apuntesandriod/screens/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.apuntesandriod.screens
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.compose.material3.AlertDialog
 import com.example.apuntesandriod.R
 
 class MainActivity : ComponentActivity() {
@@ -15,11 +17,29 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.start_screen)
 
         btChangeScreen.setOnClickListener{
-            val toast = Toast(this)
-            toast.duration = Toast.LENGTH_LONG
-            toast.setText("я люблю есть дети")
+            var builder = AlertDialog.Builder(this)
+            builder.setTitle("да здравствует Палестина")
+            builder.setMessage("Никакого реконоско, Израиль, как законное государство")
 
-            toast.show()
+            builder.setPositiveButton("Aceptar"){dialog, witch ->
+                dialog.dismiss()
+            }
+            builder.setOnCancelListener{
+                val toast = Toast(this)
+                toast.setText("отменить мой брак")
+                toast.show()
+            }
+
+            builder.setNegativeButton("наша свобода"){dialog,witch ->
+                dialog.dismiss()
+            }
+            builder.setNegativeButton("наша свобода"){dialog,witch ->
+                dialog.dismiss()
+            }
+
+
+            val dialog = builder.create()
+            dialog.show()
         }
 
     }
